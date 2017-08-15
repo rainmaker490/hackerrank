04152c959938871e88215b34fb59b76b218065a5
Varun Patel <varun@verve3.io>
2017-08-15 13:36:13 -0700

    Greedy - Jim and the Orders

--- main.c
+++ main.c
@@ -823,3 +823,49 @@ int main() {
     printf("%d",unfairness);
     return 0;
 } */
+
+
+/*
+//Jim and the Orders
+typedef struct {
+    int data;
+    int index;
+} Orders;
+
+int comparator (const void *a, const void *b){
+    Orders *aOrder = (Orders*)a;
+    Orders *bOrder = (Orders*)b;
+    int aData = aOrder->data;
+    int bData = bOrder->data;
+    int retVal;
+    if (aData - bData == 0){
+        retVal = aOrder->index - bOrder->index;
+    }  else {
+        retVal = aData - bData;
+    }
+    return aData - bData;
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    Orders *arr = malloc(sizeof(Orders)*n);
+    int input1;
+    int input2;
+    for (int i = 0; i < n; i++){
+        scanf("%d %d",&input1, &input2);
+        arr[i].data = input1+input2;
+        arr[i].index = i+1;
+    }
+    //for (int i = 0; i < n;i++){
+    //    printf("%d ", arr[i].data);
+    //}
+    qsort(arr,n,sizeof(Orders),comparator);
+    
+    for (int i = 0; i < n;i++){
+        int j = i + 1;
+        printf("%d ", arr[i].index);
+    }
+    free(arr);
+    return 0;
+} */

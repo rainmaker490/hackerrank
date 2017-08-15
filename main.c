e6cc401bece1584a1252b22a66c9dfe65cf1a5f8
Varun Patel <varun@verve3.io>
2017-08-15 11:04:16 -0700

    Greedy Florist

--- main.c
+++ main.c
@@ -760,3 +760,34 @@ int main() {
     printf("%d\n", result);
     return 0;
 } */ 
+
+/*
+//Greedy Florist
+int comparator (const void* a, const void *b){
+    return *(int*)b - *(int*)a;
+}
+
+int getMinimumCost(int n, int k, int* c){
+    qsort(c,n,sizeof(int),comparator);
+    int minimumCost = 0;
+    for(int i = 0,coefficient = 0; i < n; i++){
+        if (i%k == 0) {
+            coefficient++;
+        }
+        minimumCost += (coefficient)*c[i];
+    }
+    return minimumCost;
+}
+
+int main() {
+    int n;
+    int k;
+    scanf("%d %d", &n, &k);
+    int *c = malloc(sizeof(int) * n);
+    for(int c_i = 0; c_i < n; c_i++){
+        scanf("%d",&c[c_i]);
+    }
+    int minimumCost = getMinimumCost(n, k, c);
+    printf("%d\n", minimumCost);
+    return 0;
+} */

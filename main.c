5f21d685189fc39c37531bf4158d9c26ab493fc7
Varun Patel <varun@verve3.io>
2017-08-15 11:44:02 -0700

    Greedy - Max Min

--- main.c
+++ main.c
@@ -791,3 +791,35 @@ int main() {
     printf("%d\n", minimumCost);
     return 0;
 } */
+
+/*
+//Max Min
+int comparator(const void* a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+
+int main() {
+    int n, k;
+    scanf("%d\n%d", &n,&k);
+    
+    int *list = malloc(sizeof(int)*n);
+    for (int i = 0; i < n; i++){
+        scanf("%d",&list[i]);
+    }
+    qsort(list,n,sizeof(int),comparator);
+    //for (int i = 0; i < n; i++){
+    //    printf("%d, ",list[i]);
+    //}
+    //printf("\n");
+    int unfairness = list[n-1]-list[0];
+    for (int i = 0; i <= n-k; i++){
+        //printf("%d\n",n-k);
+        if (list[i+k-1]-list[i] < unfairness) {
+            //printf("%d:%d %d:%d\n", i,list[i],i+k,list[i+k-1]);
+            unfairness = list[i+k-1]-list[i];
+        }
+    }
+    free(list);
+    printf("%d",unfairness);
+    return 0;
+} */

59de56e831e1591cb33ba13a0b61dbb65cb055e3
Varun Patel <varun@verve3.io>
2017-08-15 21:00:25 -0700

    Warmup - Simple Array Sum

---
+++ Algorithms/Warmup/simpleArraySum.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int simpleArraySum(int ar_size, int* ar) {
+    int total = 0;
+    for (int i = 0; i < ar_size; i++){
+        total += ar[i];
+    }
+    return total;
+}
+
+int main() {
+    int n;
+    scanf("%i", &n);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        scanf("%i",&ar[ar_i]);
+    }
+    int result = simpleArraySum(n, ar);
+    free(ar);
+    printf("%d\n", result);
+    return 0;
+}

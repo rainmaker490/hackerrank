23af3ef1011bcce725e5010df37678d20824f6ec
rainmaker490 <varun@verve3.io>
2017-08-16 15:49:45 -0700

    Implementation - Utopian Tree

---
+++ Algorithms/Implementation/utopianTree.java
@@ -0,0 +1,16 @@
+//@author: Varun Patel
+//Utopian Tree
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numCases = in.nextInt();
+        for (int i = 0; i < numCases; i++) {
+            int n = in.nextInt();
+            int answer = ((int) Math.pow(2, (n + 3) / 2)) + (((int) Math.pow(-1, n)) - 3) / 2;
+            System.out.println(answer);
+        }
+    }
+}

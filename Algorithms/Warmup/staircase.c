112b70a3f7d8ae3ed1b0f528943dd8303e1f224e
Varun Patel <varun@verve3.io>
2017-08-15 22:05:29 -0700

    Warmup - Staircase

---
+++ Algorithms/Warmup/staircase.c
@@ -0,0 +1,25 @@
+//@author: Varun Patel
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++){
+        //Print Spaces:
+        for (int spacePrint = 0; spacePrint < n - i -1; spacePrint++) {
+            printf(" ");
+        }
+        //Print Hashes:
+        for (int hashPrint = 0; hashPrint < i+1; hashPrint++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}

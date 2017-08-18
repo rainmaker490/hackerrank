6b0f0b65c7c1ec0047490734594212da10372a4b
rainmaker490 <varun@verve3.io>
2017-08-18 14:57:50 -0700

    [WIP] Dynamic Programming - Equal

--- Algorithms/Dynamic Programming/equal.c
+++ Algorithms/Dynamic Programming/equal.c
@@ -8,7 +8,34 @@
 #include <limits.h>
 #include <stdbool.h>
 
+/* Algorithm / Example :
+2 (7) 7 7 7 7
+7 7 (12) 12 12 12
+12 12 12 (19) 19 19
+17 17 17 (19) 24 24
+19 19 19 (19) 26 26
+ */
+
+/* Input :
+1
+4
+2 2 3 7 
+ */
+
+int getMinimumIterations(int **distributedCandies, int index, int arrayLength) {
+    
+}
+
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int queries;
+    scanf("%d",&queries);
+    for (int i = 0; i < queries; i++) {
+        int arrayLength;
+        scanf("%d", &arrayLength);
+        int *distributedCandies = malloc(arrayLength*sizeof(int));
+        int minimumIterations = getMinimumIterations(&distributedCandies,0,arrayLength);
+        printf("%d\n",minimumIterations);
+        free(distributedCandies);
+    }
     return 0;
 }

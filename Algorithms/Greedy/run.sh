b2cac2c284543e455e6a1e415dfc841f74d01c35
Varun Patel <varun@verve3.io>
2017-08-24 22:16:16 -0700

    Run Script - Add for Greedy Subdirectory

---
+++ Algorithms/Greedy/run.sh
@@ -0,0 +1,7 @@
+#Sample Run Script for C Colutions
+#Greedy Subdirectory
+#@author : Varun Patel
+rm -rf output.txt
+gcc -o minimumAbsoluteDifferenceinAnArray minimumAbsoluteDifferenceinAnArray.c
+./minimumAbsoluteDifferenceinAnArray < input.txt >> output.txt
+open output.txt

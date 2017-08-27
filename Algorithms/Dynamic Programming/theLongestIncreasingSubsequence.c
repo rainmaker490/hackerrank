bb51acc2c096087f80569a04f2fdf3bb929bbbf2
Varun Patel <varun@verve3.io>
2017-08-26 19:13:12 -0700

    Dynamic Programming - The Longest Increasing Subsequence

--- Algorithms/Dynamic Programming/theLongestIncreasingSubsequence.c
+++ Algorithms/Dynamic Programming/theLongestIncreasingSubsequence.c
@@ -16,21 +16,43 @@
  3
  */
 
-//TODO:
-int getLongestIncreasingSubsequence() {
-    return 0;
+//Algorithm Reference : https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+//O(nlogn) solution (binary search through tail Array);
+//O(n^2) if bruteforce search through tail Array;
+
+int binarySearch(int *lisTailArray, int lisArraySize, int searchNumber) {
+    int leftPivot = 0;
+    int rightPivot = lisArraySize;
+    while(rightPivot-leftPivot > 1) {
+        int middlePivot = leftPivot + (rightPivot - leftPivot)/2;
+        if (lisTailArray[middlePivot] >= searchNumber){
+            rightPivot = middlePivot;
+        } else {
+            leftPivot = middlePivot;
+        }
+    }
+    return rightPivot;
 }
 
 int main() {
     int queries;
     scanf("%d",&queries);
     
-    int *lisArray = malloc(queries*sizeof(int));
-    for(int i = 0; i < queries; i++) {
-        scanf("%d",&lisArray[i]);
-    }
+    int *lisTailArray = calloc(queries+1,sizeof(int));
     
-    int lis = getLongestIncreasingSubsequence();
-    printf("%d",lis);
+    int currentIndex = 0;
+    int input;
+    for(int i = 0; i <= queries; i++) {
+        scanf("%d",&input);
+        if(lisTailArray[currentIndex] < input) {
+            currentIndex++;
+            lisTailArray[currentIndex] = input;
+        } else if(lisTailArray[currentIndex] > input) {
+            int index = binarySearch(lisTailArray, currentIndex, input);
+            lisTailArray[index] = input;
+        }
+    }
+    printf("%d",currentIndex);
+    free(lisTailArray);
     return 0;
 }

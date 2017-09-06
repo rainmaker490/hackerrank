500a0ddbda047ebf8cbe832f1b393834ef07b5b2
Varun Patel <varun@verve3.io>
2017-09-05 22:37:04 -0700

    Dynamic Programming - Summing Pieces Memoized Solution

--- Algorithms/Dynamic Programming/summingPieces.m
+++ Algorithms/Dynamic Programming/summingPieces.m
@@ -29,47 +29,70 @@
 136
  */
 
-int getSummedPieces(int *summingPiecesArray,int startIndex, int endIndex) {
+/* INPUT / OUTPUT
+ 3
+ 1 3 6
+ ------->
+ 73
+ */
+
+long long int getSummedPieces(int *summingPiecesArray,int startIndex, int endIndex, NSMutableDictionary **memo) {
     if (startIndex > endIndex) {
         //printf("Iterator  %d\n",startIndex-endIndex);
         return 0;
     }
     
+    if ([*memo objectForKey:[NSString stringWithFormat:@"%d",startIndex]]) {
+        //printf("VALUE: %lli\n",[[*memo objectForKey:[NSString stringWithFormat:@"%d",startIndex]] longLongValue]);
+        return [[*memo objectForKey:[NSString stringWithFormat:@"%d",startIndex]] longLongValue];
+    }
+    
     int groups = 1;
-    int currentSum = 0;
+    long long int currentSum = 0;
     //printf("StartIndex:%d,EndIndex:%d\n",startIndex,endIndex);
     int loopCount = 1;
     while (groups - 1 < endIndex - startIndex + 1) {
-        //printf("Groups:%d\n",groups);
-        int groupedSum = 0;
+        //printf("Groups:%d, End:%d, Start:%d\n",groups - 1,endIndex,startIndex+1);
+        long long int groupedSum = 0;
         for (int i = startIndex; i <= startIndex+groups-1; i++){
             groupedSum+=summingPiecesArray[i];
+            groupedSum = groupedSum % 1000000007;
         }
+        //printf("GroupedSum %lli\n",groupedSum);
         /*
         for (int i = startIndex; i <= startIndex+groups-1; i++){
             printf("%d, ",summingPiecesArray[i]);
         }
         printf("\n");
-         */
-        
-        int subSum = getSummedPieces(summingPiecesArray,startIndex+groups,endIndex);
+        */
+        long long int subSum = getSummedPieces(summingPiecesArray,startIndex+groups,endIndex,memo);
         //printf("GroupedSum%d\n",groupedSum);
         //printf("GroupedTotal%d\n",groupedSum*groups);
-        currentSum += subSum;
-        int loopCount = endIndex - (startIndex + groups) ;
+        subSum += 1000000007;
+        currentSum += subSum % 1000000007;
+        currentSum = currentSum % 1000000007;
+        long long int loopCount = endIndex - (startIndex + groups) ;
         //printf("LoopCount   %d\n",loopCount);
         if (loopCount < 0) {
             loopCount++;
             //printf("LoopCount   %d\n",loopCount);
             //printf("Loop Count Less Than 0\n");
         }
-        //printf("GroupedSum: %d ,,,, LoopCount   %d \n",groupedSum,loopCount);
-        currentSum += (groupedSum * pow(2,loopCount) * groups);
+        //printf("GroupedSum: %lli ,,,, LoopCount   %lli \n",groupedSum,loopCount);
+        long long int pow = 1;
+        for (int i = 1; i <= loopCount; i++) {
+            pow *= 2;
+            pow = pow % 1000000007;
+        }
+        int sum = (groupedSum * pow * groups) % 1000000007;
+        currentSum +=(sum+1000000007);
+        currentSum = currentSum % 1000000007;
         //printf("CURRENTSUM   %d\n",currentSum);
         //printf("Iterator  %d\n",loopCount);
         //printf("GroupedSum%d\n",groupedSum);
         groups++;
     }
+    [*memo setObject:@(currentSum) forKey:[NSString stringWithFormat:@"%d",startIndex]];
     return currentSum;
 }
 
@@ -81,7 +104,8 @@ int main(int argc, const char *argv[]) {
     for (int i = 0; i < arrayLength; i++) {
         scanf("%i",&summingPiecesArray[i]);
     }
-    printf("%i\n",getSummedPieces(summingPiecesArray,0,arrayLength-1));
+    NSMutableDictionary *memo = [NSMutableDictionary dictionary];
+    printf("%lli\n",getSummedPieces(summingPiecesArray,0,arrayLength-1, &memo));
     [pool drain];
     return 0;
 }
